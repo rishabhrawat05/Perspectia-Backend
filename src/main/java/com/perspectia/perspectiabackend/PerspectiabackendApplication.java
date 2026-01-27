903bd2f964805c8b8c90d5e35782183c23065367
Rishabh <rishabhrawat.171105@gmail.com>
2026-01-27 20:49:15 +0530

    Fixed 403 exception

--- src/main/java/com/perspectia/perspectiabackend/PerspectiabackendApplication.java
+++ src/main/java/com/perspectia/perspectiabackend/PerspectiabackendApplication.java
@@ -2,10 +2,12 @@ package com.perspectia.perspectiabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class PerspectiabackendApplication {
 
 	public static void main(String[] args) {
